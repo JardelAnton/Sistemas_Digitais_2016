46ec5d3ecf7add37bc1344cf9fa4cb00b890badd
JardelAnton <jardellanton@gmail.com>
2016-03-03 05:27:26 -0300

    codigo feito

--- pisca.v
+++ pisca.v
@@ -1,5 +1,27 @@
 module pisca (
-		input clk,
-		output led,
-		input pow
+	input clk,
+	output led
 );
+	reg x;
+	assign led = x;
+
+	always @(posedge clk) begin
+		x <= ~x;
+	end
+endmodule
+
+module teste;
+	reg clk;
+	wire led;
+
+	always #2 clk = ~clk;
+	pisca p(clk, led);
+
+	initial begin
+		$dumpvars(0, p);
+		#0
+		clk <= 1'b0;
+		#500;
+		$finish;
+	end
+endmodule
