43234c51363219d17157cf991e7310153f2ca9a2
JardelAnton <jardellanton@gmail.com>
2016-03-17 01:43:26 -0300

    Update trab2.v

--- trab2.v
+++ trab2.v
@@ -2,18 +2,16 @@ module trab2(
 	input clock_50,
 	output [0:7]LEDG
 );
-	reg[31:0] cont = 0;
+	reg[31:0] c = 0;
 	reg light_on = 1;
 
 	assign  = LEDG[0]= ~ light_on;
 
-	always @(posedge clk) begin
-			cont <=cont+1;
+	always @(posedge clock_50) begin
+			c <=cont+1;
 		if (cont == 50000000) begin
 			light_on  = ~light_on;
-			cont <= 0;
+			c <= 0;
 		end
-		
-		
 	end
 endmodule
